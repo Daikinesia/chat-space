11e832d4580aa670409a16d3f239a3c9be943001
Daiki.Ishikawa <ishikawadaiki@gmail.com>
2019-10-20 18:02:12 +0900

    Modify to Activate Capistrano

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,29 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
 app_path = File.expand_path('../../', __FILE__)
 
+worker_processes 1
+
+working_directory app_path
+pid "#{app_path}/tmp/pids/unicorn.pid"
+listen "#{app_path}/tmp/sockets/unicorn.sock"
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+stdout_path "#{app_path}/log/unicorn.stdout.log"
+
+ ↓↓↓↓↓↓↓ 以下のように変更 ↓↓↓↓↓↓
+
+# ../が一つ増えている
+app_path = File.expand_path('../../../', __FILE__)
+
+worker_processes 1
+# currentを指定
+working_directory "#{app_path}/current"
+
+# それぞれ、sharedの中を参照するよう変更
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+
 #アプリケーションサーバの性能を決定する
 worker_processes 1
 
